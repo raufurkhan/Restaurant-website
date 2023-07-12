7e820782dd28b080022f5e71b5cb1e204c79f0bb
raufurkhan <raufurkhan82@gmail.com>
2023-07-12 23:48:58 +0530

    Add Restaurant Summary

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,49 @@
+
+import classes from './AvailableMeals.module.css'
+
+
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Schnitzel',
+      description: 'A german specialty!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Green Bowl',
+      description: 'Healthy...and green...',
+      price: 18.99,
+    },
+  ];
+
+  const AvailableMeals=()=>{
+
+const mealItems=DUMMY_MEALS.map((item)=>{
+    return <li>{item.name}</li>
+})
+
+return (
+    <section className={classes.meal}>
+<ul>
+    {mealItems}
+</ul>
+</section>
+)
+
+
+  }
+
+  export default AvailableMeals
