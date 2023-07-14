5a11146ad48e3718e4c63a505ed2841c013d41ca
raufurkhan <raufurkhan82@gmail.com>
2023-07-15 03:25:31 +0530

    Making the items visible

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,49 +1,53 @@
-
-import classes from './AvailableMeals.module.css'
-
+import Card from "../UI/Card";
+import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
-  const AvailableMeals=()=>{
+const AvailableMeals = () => {
+  const mealItems = DUMMY_MEALS.map((item) => {
+    return (
+      <MealItem
+        key={item.id}
+        name={item.name}
+        description={item.description}
+        price={item.price}
+      ></MealItem>
+    );
+  });
 
-const mealItems=DUMMY_MEALS.map((item)=>{
-    return <li>{item.name}</li>
-})
+  return (
+    <section className={classes.meals}>
+      <Card>
+      <ul>{mealItems}</ul>
+      </Card>
+    </section>
+  );
+};
 
-return (
-    <section className={classes.meal}>
-<ul>
-    {mealItems}
-</ul>
-</section>
-)
-
-
-  }
-
-  export default AvailableMeals
+export default AvailableMeals;
